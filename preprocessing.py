bbc941e10a0b855218d73bf3f1768eba347740c5
Szymon Drenda <szymondrenda2@gmail.com>
2020-03-30 17:45:56 +0200

    Improve voronoi plot, add simple DiagramConsolidator class, fix random sampler max points checking

--- preprocessing.py
+++ preprocessing.py
@@ -1,36 +1,17 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-
-# class DiagramScaler(BaseEstimator, TransformerMixin):
-#     """
-#     This is a class for diagram transformation to be in range [0, 1] x [0, 1] with the respect 
-#     to maximum value of all (x, y) column of all given diagrams.
-#     """
-#     def __init__(self):
-#         return None
-    
-#     def fit(self, X, y=None):
-#         diagram_column_maxes = [np.max(diagram, axis=0) for diagram in X]
-#         self.all_diag_max = np.max(diagram_column_maxes, axis=0)
-#         return self
-    
-#     def transform(self, X):
-#         return X / self.all_diag_max
-		
 		
 class RandomPDSampler(BaseEstimator, TransformerMixin):
     def __init__(self, max_points=None, weight_function=None):
         self.max_points = max_points
         self.weight_function = weight_function
         
-        
-    
     def fit(self, X, y=None):
         return self
     
     def transform(self, X):
         out_diagram = np.concatenate(X)
-        if not self.max_points or len(out_diagram[0]) <= self.max_points:
+        if not self.max_points or len(out_diagram) <= self.max_points:
             return [out_diagram]
         
         p = None
@@ -42,3 +23,13 @@ class RandomPDSampler(BaseEstimator, TransformerMixin):
         choice = np.random.choice(len(out_diagram), self.max_points, p=p, replace=False)
         
         return [out_diagram[choice]]
+
+class DiagramConsolidator(BaseEstimator, TransformerMixin):
+    '''
+    Simple class taking list of persistence diagrams and returning single consolidated diagram
+    '''
+    def fit(self, X, y=None):
+        return self
+        
+    def transform(self, X):
+        return np.concatenate(X)
--- visualization.py
+++ visualization.py
@@ -1,7 +1,9 @@
 import matplotlib.cm
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
 from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial._plotutils import _held_figure
 
 
 def plot_persistence_bow(pd, bow):
@@ -12,5 +14,60 @@ def plot_persistence_bow(pd, bow):
 
     plt.bar(bins, histogram, color=colors)
 
-def plot_voronoi(bow):
-    return voronoi_plot_2d(Voronoi(bow.cluster_centers_))
+
+# Copied from scipy.spatial._plotutils and modified
+def plot_voronoi(bow, plot_range=(1, 1), ax=None, **kw):
+    if ax is None:
+        ax = plt.axes()
+
+    plot_range = np.array(plot_range)
+    vor = Voronoi(bow.cluster_centers_)
+
+    if vor.points.shape[1] != 2:
+        raise ValueError("Voronoi diagram is not 2-D")
+
+    if kw.get('show_points', True):
+        point_size = kw.get('point_size', None)
+        ax.plot(vor.points[:, 0], vor.points[:, 1], 'x', markersize=point_size, color = (0,0,0))
+
+    line_colors = kw.get('line_colors', 'k')
+    line_width = kw.get('line_width', 1.0)
+    line_alpha = kw.get('line_alpha', 1.0)
+
+    center = vor.points.mean(axis=0)
+
+    finite_segments = []
+    infinite_segments = []
+    for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
+        simplex = np.asarray(simplex)
+        if np.all(simplex >= 0):
+            finite_segments.append(vor.vertices[simplex])
+        else:
+            i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
+
+            t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])  # normal
+
+            midpoint = vor.points[pointidx].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[i] + direction * \
+                2  # to guarantee that coords > 1
+
+            infinite_segments.append([vor.vertices[i], far_point])
+
+    ax.add_collection(LineCollection(finite_segments,
+                                     colors=line_colors,
+                                     lw=line_width,
+                                     alpha=line_alpha,
+                                     linestyle='solid'))
+    ax.add_collection(LineCollection(infinite_segments,
+                                     colors=line_colors,
+                                     lw=line_width,
+                                     alpha=line_alpha,
+                                     linestyle='dashed'))
+
+    ax.set_xlim(0, plot_range[0])
+    ax.set_ylim(0, plot_range[1])
+
+    return ax
