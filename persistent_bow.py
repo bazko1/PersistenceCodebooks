40987aed0ef74a578bc126afd44c849ff196b654
bazyli <bazyli.kot@student.uj.edu.pl>
2020-04-15 22:49:49 +0200

    moved BPTransform and Scaling to PBow

--- persistent_bow.py
+++ persistent_bow.py
@@ -1,9 +1,21 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin, ClusterMixin
+from gudhi.representations.preprocessing import BirthPersistenceTransform, DiagramScaler
+from sklearn.preprocessing import MaxAbsScaler
+
 
 class PersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
-    def __init__(self, cluster, sampler=None):
+
+    def __init__(self,
+                 cluster,
+                 *,
+                 transformator = BirthPersistenceTransform(),
+                 scaler = DiagramScaler(use=True, scalers=[((0,), MaxAbsScaler(copy=False)), ((1,), MaxAbsScaler(copy=False))]),
+                 sampler = None
+                 ):
         self.cluster = cluster
+        self.transformator = transformator
+        self.scaler = scaler
         self.sampler = sampler
 
     @property
@@ -11,6 +23,11 @@ class PersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
         return self.cluster.n_clusters
 
     def fit(self, X, y=None, sample_weight=None):
+        # TODO: To be discussed, is it enough repeated operations that we should prepare Pipeline[transformator, scaler?
+        if self.transformator:
+            self.transformator.fit(X, y)
+        if self.scaler:
+            self.scaler.fit(X, y)
         if self.sampler:
             X = self.sampler.fit_transform(X, y)
         X = np.concatenate(X)
@@ -29,6 +46,11 @@ class PersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
         Returns list of bags-of-words
         '''
         out = []
+        if self.transformator:
+            X = self.transformator.transform(X)
+        if self.scaler:
+            X = self.scaler.transform(X)
+
         for diagram in X:
             pred = self.cluster.predict(diagram)
             histogram = np.bincount(pred, minlength=self.n_clusters)
