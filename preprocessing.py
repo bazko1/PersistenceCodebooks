d49f1c3bb5d124c6c7066e982626b629ab278dd0
Szymon Drenda <szymondrenda2@gmail.com>
2020-04-19 17:34:10 +0200

    Implement random state for RandomPDSampler

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,20 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import validation
 
 
 class RandomPDSampler(BaseEstimator):
-    def __init__(self, max_points=None, weight_function=None):
+    def __init__(self, max_points=None, weight_function=None, random_state=None):
         self.max_points = max_points
         self.weight_function = weight_function
+        self.random_state = random_state
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
+        rnd = validation.check_random_state(self.random_state)
+
         out_diagram = np.concatenate(X)
         if not self.max_points or len(out_diagram) <= self.max_points:
             return [out_diagram]
@@ -21,7 +25,8 @@ class RandomPDSampler(BaseEstimator):
             p = np.vectorize(self.weight_function)(persistencies)
             p /= np.sum(p)
 
-        choice = np.random.choice(len(out_diagram), self.max_points, p=p, replace=False)
+
+        choice = rnd.choice(len(out_diagram), self.max_points, p=p, replace=False)
 
         return [out_diagram[choice]]
 
