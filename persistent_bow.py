eb8a15b20cbb4f641012c8499fc6cc1259d78487
Szymon Drenda <szymondrenda2@gmail.com>
2020-04-19 17:34:35 +0200

    Implement StablePBoW + Formatting

--- persistent_bow.py
+++ persistent_bow.py
@@ -5,13 +5,12 @@ from sklearn.preprocessing import MaxAbsScaler
 
 
 class PersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
-
     def __init__(self,
                  cluster,
                  *,
-                 transformator = BirthPersistenceTransform(),
-                 scaler = DiagramScaler(use=True, scalers=[((0,), MaxAbsScaler(copy=False)), ((1,), MaxAbsScaler(copy=False))]),
-                 sampler = None
+                 transformator=BirthPersistenceTransform(),
+                 scaler=DiagramScaler(use=True, scalers=[((0,), MaxAbsScaler(copy=False)), ((1,), MaxAbsScaler(copy=False))]),
+                 sampler=None
                  ):
         self.cluster = cluster
         self.transformator = transformator
@@ -62,3 +61,59 @@ class PersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
 
     def fit_predict(self, X, y=None, sample_weight=None):
         return self.fit(X, y, sample_weight).predict(X, sample_weight)
+
+
+class StablePersistentBow(BaseEstimator, TransformerMixin, ClusterMixin):
+    def __init__(self,
+                 mixture,
+                 *,
+                 transformator=BirthPersistenceTransform(),
+                 scaler=DiagramScaler(use=True, scalers=[((0,), MaxAbsScaler(copy=False)), ((1,), MaxAbsScaler(copy=False))]),
+                 sampler=None):
+        self.mixture = mixture
+        self.transformator = transformator
+        self.scaler = scaler
+        self.sampler = sampler
+
+    def fit(self, X, y=None):
+        if self.transformator:
+            X = self.transformator.fit_transform(X, y)
+        if self.scaler:
+            X = self.scaler.fit_transform(X, y)
+        if self.sampler:
+            X = self.sampler.fit_transform(X, y)
+
+        X = np.concatenate(X)
+        self.mixture.fit(X, y)
+
+        return self
+
+    def predict(self, X, sample_weight=None):
+        out = []
+        for diagram in X:
+            out.append(self.mixture.predict(diagram, sample_weight))
+
+        return np.array(out)
+
+    def transform(self, X):
+        '''
+        Returns list of bags-of-words
+        '''
+        out = []
+        if self.transformator:
+            X = self.transformator.transform(X)
+        if self.scaler:
+            X = self.scaler.transform(X)
+
+        for diagram in X:
+            probabilities = self.mixture.predict_proba(diagram)
+            histogram = np.sum(probabilities, axis=0)
+            out.append(histogram)
+
+        return np.array(out)
+
+    def fit_transform(self, X, y=None):
+        return self.fit(X, y).transform(X)
+
+    def fit_predict(self, X, y=None):
+        return self.fit(X, y).predict(X)
