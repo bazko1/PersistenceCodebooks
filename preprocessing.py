5dc93d4d0698cbb14ba1cec538add453cbb32a9b
Szymon Drenda <szymondrenda2@gmail.com>
2020-07-17 22:18:46 +0200

    Grid sampler rewrite, max_points is global, not per cell

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ def _sample(X, max_points=None, weight_function=None, random_state=None):
     rnd = validation.check_random_state(random_state)
     rows = X.shape[0]
 
-    if not max_points or rows <= max_points:
+    if max_points is None or rows <= max_points:
         return X
 
     p = None
@@ -17,6 +17,25 @@ def _sample(X, max_points=None, weight_function=None, random_state=None):
         p /= np.sum(p)
 
     return X[rnd.choice(rows, max_points, p=p, replace=False)]
+
+def _grid_generator(X, y_points, x_points):
+    for y in range(1, len(y_points)):
+        if y == 1:
+            mask = y_points[y-1] <= X[:, 1]
+        else:
+            mask = y_points[y-1] <  X[:, 1]
+        mask &= X[:, 1] <= y_points[y]
+        y_split = X[mask]
+
+        for x in range(1, len(x_points)):
+            if x == 1:
+                mask = x_points[x-1] <= y_split[:, 0]
+            else:
+                mask = x_points[x-1] <  y_split[:, 0]
+            mask &= y_split[:, 0] <= x_points[x]
+
+            yield y_split[mask]
+
 
 class RandomPDSampler(BaseEstimator):
     def __init__(self, max_points=None, weight_function=None, random_state=None):
@@ -50,37 +69,46 @@ class GridPDSampler(BaseEstimator):
     
     def transform(self, X):
         out = []
-        
         X = np.concatenate(X)
         y_points = np.linspace(np.min(X[:, 1]), np.max(X[:, 1]), self.grid_shape[0] + 1)
         x_points = np.linspace(np.min(X[:, 0]), np.max(X[:, 0]), self.grid_shape[1] + 1)
-        
-        #Create and sample grids
-        for y in range(1, len(y_points)):
-            if y == 1:
-                indices = y_points[y-1] <= X[:, 1]
+
+        cells_populations, _, _ = np.histogram2d(x=X[:,0], y=X[:,1], bins=(x_points, y_points))
+        cells_populations = cells_populations.T
+        samples_to_take = np.zeros(cells_populations.shape, dtype=np.int32)
+        points_to_distribute = self.max_points
+
+        sorting_indices = np.unravel_index(
+            cells_populations.argsort(axis=None),
+            cells_populations.shape)
+        cells_left = cells_populations.size
+
+        #Distribute samples to cells, moving leftover samples uniformly to rest of cells
+        for cell_indices in np.column_stack(sorting_indices):
+            y_i, x_i = cell_indices
+            population = cells_populations[y_i, x_i]
+            samples = points_to_distribute // cells_left
+            if population < samples:
+                points_to_distribute -= population
+                samples_to_take[y_i, x_i] = population
             else:
-                indices = y_points[y-1] <  X[:, 1]
-            indices &= X[:, 1] <= y_points[y]
-            y_split = X[indices]
+                points_to_distribute -= samples
+                samples_to_take[y_i, x_i] = samples
+            cells_left -= 1
 
-            for x in range(1, len(x_points)):            
-                if x == 1:
-                    indices = x_points[x-1] <= y_split[:, 0]
-                else:
-                    indices = x_points[x-1] <  y_split[:, 0]
-                indices &= y_split[:, 0] <= x_points[x]
-                grid = y_split[indices]
-
-                out.append(
-                    _sample(
-                        grid, 
-                        self.max_points, 
-                        self.weight_function, 
-                        self.random_state
-                    )
+        #Sample each grid cell
+        for grid_cell, samples in zip(
+                _grid_generator(X, y_points, x_points),
+                samples_to_take.flat):
+            out.append(
+                _sample(
+                    grid_cell,
+                    samples,
+                    self.weight_function,
+                    self.random_state
                 )
-        
+            )
+
         return [np.concatenate(out)]
                 
     def fit_transform(self, X, y=None):
